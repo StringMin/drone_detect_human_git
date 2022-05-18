18a9fec3e60ba5e10b1888a6157efce565decc35
minsoo <bellamy122@naver.com>
2022-05-18 17:25:48 +0900

    test and add git ignore

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 드론 인명 구조 관련 코드 작성 되는 곳~~
